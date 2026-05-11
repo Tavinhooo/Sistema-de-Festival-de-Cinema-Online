c8367d31017fd3fcf84053cb7abccddfc148b515
André Pereira <andre.a.pereira2425@gmail.com>
2026-05-11 15:16:16 +0100

    refactor: Move member properties from Visitante to Membro class for better separation of concerns

--- ProjetoES.API/Models/Visitante.cs
+++ ProjetoES.API/Models/Visitante.cs
@@ -3,10 +3,7 @@ namespace ProjetoES.API.Models
     public abstract class Visitante
     {
         public int Id { get; set; }
-        public string PrimeiroNome { get; set; } = string.Empty;
-        public string UltimoNome { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-        public string PasswordHash { get; set; } = string.Empty;
+        // Marca se a visita atual está autenticada.
         public bool IsLogged { get; set; }
     }
 }
